061d3fd23869b2a4f5386f8042ee3ef14d5dbc60
danugans <mas.danu29@gmail.com>
2022-02-04 11:03:13 +0700

    update b30

--- src/GuestGreeting.js
+++ src/GuestGreeting.js
@@ -1,13 +1,16 @@
 //import useEffect here
+import { useEffect } from "react";
 
 export default function GuestGreeting() {
+  // Create DidMount & WillUnmount with useEffect
 
-    // Create DidMount & WillUnmount with useEffect 
-    // inside it can print "Guest Greeting Component Did Mount" & "Guest Greeting Component Will Unmount"
+  // inside it can print "Guest Greeting Component Did Mount" & "Guest Greeting Component Will Unmount"
+  useEffect(() => {
+    console.log("Guest greeting component did mount");
+    return () => {
+      console.log("App greeting willUnmount");
+    };
+  }, []);
 
-    return (
-        <div className="text-center h1 bg-secondary text-light py-5">
-            Please Login !
-        </div>
-    )
+  return <div className="text-center h1 bg-secondary text-light py-5">Please Login !</div>;
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,13 +1,16 @@
 //import useEffect here
 
+import { useEffect } from "react";
+
 export default function Welcome() {
+  // Create DidMount & WillUnmount with useEffect
+  // inside it can print "Welcome Component Did Mount" & "Welcome Component Will Unmount"
+  useEffect(() => {
+    console.log("welcome component didmount");
+    return () => {
+      console.log("welcome component willunmount");
+    };
+  }, []);
 
-    // Create DidMount & WillUnmount with useEffect 
-    // inside it can print "Welcome Component Did Mount" & "Welcome Component Will Unmount"
-
-    return (
-        <div className="vh-100 bg-warning d-flex justify-content-center align-items-center h1 mb-0">
-            Welcome
-        </div>
-    )
+  return <div className="vh-100 bg-warning d-flex justify-content-center align-items-center h1 mb-0">Welcome</div>;
 }
